64d1e47af3ad10331debddd415ed64ed933b4d08
Alberto Lima <alberto.btlima@outlook.com>
2024-09-17 19:10:57 -0300

    add: form

--- 02-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
+++ 02-estado-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -1,17 +1,14 @@
 import { useEffect, useState } from "react";
+import Form from "../forms/form";
 
 async function createDeck() {
-  const response = await fetch(
-    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-  );
+  const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
   const deck = await response.json();
   return deck.deck_id;
 }
 
 async function getCards(deckId) {
-  const response = await fetch(
-    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
-  );
+  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
   return await response.json();
 }
 
@@ -47,46 +44,19 @@ const DeckOfCards = () => {
     fetchData();
   }, []);
 
+  const addCard = (newCard) => {
+    console.log(newCard);
+    setDeck({
+      cards: [...deck.cards, newCard],
+    });
+  };
+
   return (
     <section>
+      <Form addCard={addCard} />
       {deck.cards.length > 0 ? <CardsList cards={deck.cards} /> : "Nenhuma carta encontrada"}
     </section>
   );
-
-  /* constructor() {
-    console.log("constructor");
-    super();
-    this.state = {
-      cards: [],
-    };
-  }
-
-  async componentDidMount() {
-    console.log("componentDidMount");
-    const deckId = await createDeck();
-    const data = await getCards(deckId);
-
-    this.setState({
-      cards: data.cards,
-    });
-  }
-
-  render() {
-    console.log("render");
-    return (
-      <section>
-        <ul>
-          {this.state.cards.map((card, index) => {
-            return (
-              <li key={index}>
-                <img src={card.image} alt={card.value} />
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    );
-  }*/
 };
 
 export default DeckOfCards;
