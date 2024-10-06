0c46462dca8f72dc0e7539b4705aad4e6c02bcc4
Alberto Lima <alberto.btlima@outlook.com>
2024-10-06 12:20:35 -0300

    new class

---
+++ 08-react-custom-hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { SaveButton } from "./components/SaveButton/Savebutton";
+import StatusBar from "./components/StatusBar/StatusBar";
+
+function App() {
+  return (
+    <>
+      <StatusBar />
+      <SaveButton />
+    </>
+  );
+}
+
+export default App;
---
+++ 08-react-custom-hooks/src/components/SaveButton/SaveButton.jsx
@@ -0,0 +1,19 @@
+import { useOnlineStatus } from "../../hooks/useOnlineStatus";
+
+function SaveButton() {
+  const isOnline = useOnlineStatus();
+
+  function handleSaveClick() {
+    console.log("Save Clicked!");
+  }
+
+  return (
+    <>
+      <button disabled={!isOnline} onClick={handleSaveClick}>
+        {isOnline ? "Save progress" : "Reconnecting..."}
+      </button>
+    </>
+  );
+}
+
+export { SaveButton };
