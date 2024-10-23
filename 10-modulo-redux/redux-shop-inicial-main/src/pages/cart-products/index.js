afde0aca090a70ea558728764f861158b36845d8
Alberto Lima <alberto.btlima@outlook.com>
2024-10-22 21:54:51 -0300

    update

--- 10-modulo-redux/redux-shop-inicial-main/src/pages/cart-products/index.js
+++ 10-modulo-redux/redux-shop-inicial-main/src/pages/cart-products/index.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { Products } from "../../components/products";
 
 const CartProducts = () => {
+  const cartProducts = useState((state) => state.cartProducts);
+
   return (
     <main>
       <h1>Cart Products</h1>
       <section>
-        <Products products={[]} />  
+        <Products products={cartProducts} />
       </section>
     </main>
   );
